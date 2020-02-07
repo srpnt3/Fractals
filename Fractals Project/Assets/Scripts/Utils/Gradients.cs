2d48f86c6afbfd343c0ae2303fdcbdef721c3c5f
VirusHD <virushd93@gmail.com>
2020-02-07 21:19:49 +0100

    just moved some files
    I'm starting to have problems describing what i did lmao

--- Fractals Project/Assets/Scripts/Utils/Gradients.cs
+++ Fractals Project/Assets/Scripts/Utils/Gradients.cs
@@ -45,7 +45,7 @@ public static class Gradients {
 				deltaH = (b.H - a.H) / (s + 1f);
 			} else {
 				deltaH = -(360f - Mathf.Abs(b.H - a.H)) / (s + 1);
-				deltaH *= Math.Sign(b.H - a.H);  // sign function for the right sign
+				deltaH *= Mathf.Sign(b.H - a.H);  // sign function for the right sign
 			}
 
 			for (int i = 0; i < s; i++) {
@@ -72,53 +72,5 @@ public static class Gradients {
 		Colors.RGBColor LCHtoRGB(Colors.LCHColor col) {
 			return col.ToLAB().ToXYZ().ToRGB();
 		}
-	}
-
-	/*public static Vector3[] CreateGradient(Vector4[] Colors, int steps) {
-
-	    // test function
-	    ColorTest();
-
-	    // create vars
-	    Gradient g = new Gradient();
-	    GradientColorKey[] c = new GradientColorKey[Colors.Length];
-	    GradientAlphaKey[] a = new GradientAlphaKey[Colors.Length];
-
-	    // create keys
-	    for (int i = 0; i < Colors.Length; i++) {
-	        c[i].color = Colors[i];
-	        c[i].time = Colors[i][3];
-	        a[i].alpha = 1.0f;
-	        a[i].time = Colors[i][3];
-	    }
-
-	    g.SetKeys(c, a);
-
-	    Vector3[] r = new Vector3[steps];
-
-	    for (int i = 0; i < steps; i++) {
-	        float p = (float)i / (steps - 1);
-	        r[i] = (Vector4)g.Evaluate(p);
-	    }
-
-	    return r;
-	}*/
-
-	/*private static void ColorTest() {
-	    for (int i = 0; i < 10; i++) {
-	        Debug.Log(RGBtoLCH(Vec3toRGB(BlackToRed[i])).ToVec3().ToString("F6"));
-	    }
-	}*/
-
-	private static Colors.RGBColor Vec3toRGB(Vector3 v) {
-		return new Colors.RGBColor(v[0] / 255, v[1] / 255, v[2] / 255);
-	}
-
-	private static Colors.LCHColor RGBtoLCH(Colors.RGBColor c) {
-		return c.ToXYZ().ToLAB().ToLCH();
-	}
-
-	private static Colors.RGBColor LCHtoRGB(Colors.LCHColor c) {
-		return c.ToLAB().ToXYZ().ToRGB();
 	}
 }
