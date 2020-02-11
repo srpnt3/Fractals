1a62a781101d2183c6cfe4504aabe9e752596e98
VirusHD <virushd93@gmail.com>
2020-02-11 22:19:04 +0100

    Just found out those are the same methods with different values lmao

--- Fractals Project/Assets/Scripts/Sierpinski/Sierpinski.cs
+++ Fractals Project/Assets/Scripts/Sierpinski/Sierpinski.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SierpinskiTriangle : App {
+public class Sierpinski : App {
+
+	[Range(1, 4)] public int n = 3;
+	[Range(1, 9)] public int iterations = 5;
 	
 	[ImageEffectOpaque]
 	private void OnRenderImage(RenderTexture s, RenderTexture d) {
@@ -12,6 +15,8 @@ public class SierpinskiTriangle : App {
 		// shader
 		shader.SetTexture(0, "Texture", tex);
 		shader.SetTexture (0, "Source", s);
+		shader.SetInt("N", n);
+		shader.SetInt("Iterations", iterations);
 		
 		shader.Dispatch(0, Mathf.CeilToInt(w / 8), Mathf.CeilToInt(h / 8), 1);
 		
