fa52f982cc8422bca5408ea253d5478a56accfd8
VirusHD <virushd93@gmail.com>
2020-02-11 18:31:15 +0100

    Added two 2D fractals
    Sierpinski Triangle and Sierpinski Carpet

---
+++ Fractals Project/Assets/Scripts/SierpinskiTriangle/SierpinskiTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SierpinskiTriangle : App {
+	
+	[ImageEffectOpaque]
+	private void OnRenderImage(RenderTexture s, RenderTexture d) {
+		
+		Init();
+		
+		// shader
+		shader.SetTexture(0, "Texture", tex);
+		shader.SetTexture (0, "Source", s);
+		
+		shader.Dispatch(0, Mathf.CeilToInt(w / 8), Mathf.CeilToInt(h / 8), 1);
+		
+		// apply
+		Graphics.Blit(tex, d);
+	}
+}
