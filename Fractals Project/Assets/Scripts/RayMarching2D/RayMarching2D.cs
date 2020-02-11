f69342731db24a60b63da8f26c6ba1a3143f813d
VirusHD <virushd93@gmail.com>
2020-02-11 14:23:16 +0100

    Finished RayMarching2D

--- Fractals Project/Assets/Scripts/RayMarching2D/RayMarching2D.cs
+++ Fractals Project/Assets/Scripts/RayMarching2D/RayMarching2D.cs
@@ -8,8 +8,8 @@ using Random = UnityEngine.Random;
 public class RayMarching2D : App {
 	
 	public Vector2 origin = new Vector2(960, 540);
-	[Range(0, 360)] public float direction;
-	[Range(1, 100)] public int steps;
+	[Range(0, 360)] public float direction = 0;
+	[Range(1, 100)] public int steps = 1;
 
 	private ComputeBuffer shapes;
 	private List<Shape> objects;
@@ -17,20 +17,20 @@ public class RayMarching2D : App {
 
 	private void Start() {
 		objects = new List<Shape>(); // Looks Random
-		objects.Add(new Shape(new Vector2(1290, 451), 69, 0, 0, new Vector3(204, 51, 51)));
-		objects.Add(new Shape(new Vector2(684, 669), 85, 0, 0, new Vector3(204, 51, 51)));
-		objects.Add(new Shape(new Vector2(390, 281), 89, 0, 0, new Vector3(204, 51, 51)));
-		objects.Add(new Shape(new Vector2(410, 868), 91, 1, 0, new Vector3(204, 51, 51)));
-		objects.Add(new Shape(new Vector2(1260, 756), 100, 1, 0, new Vector3(204, 51, 51)));
-		objects.Add(new Shape(new Vector2(1739, 180), 137, 1, 0, new Vector3(204, 51, 51)));
+		objects.Add(new Shape(new Vector2(1290, 451), new Vector2(69, 0), 0, 0, new Vector3(204, 51, 51)));
+		objects.Add(new Shape(new Vector2(684, 669), new Vector2(85, 0), 0, 0, new Vector3(204, 51, 51)));
+		objects.Add(new Shape(new Vector2(390, 281), new Vector2(89, 0), 0, 0, new Vector3(204, 51, 51)));
+		objects.Add(new Shape(new Vector2(410, 868), new Vector2(91, 65), 1, 0, new Vector3(204, 51, 51)));
+		objects.Add(new Shape(new Vector2(1260, 756), new Vector2(100, 100), 1, 0, new Vector3(204, 51, 51)));
+		objects.Add(new Shape(new Vector2(1739, 180), new Vector2(137, 97), 1, 0, new Vector3(204, 51, 51)));
 	}
 
 	[ImageEffectOpaque]
 	private void OnRenderImage(RenderTexture s, RenderTexture d) {
 		
 		Init();
-		//March();
-		Min(origin);
+		March();
+		//Min(origin);
 		InitShapes();
 
 		// shader
@@ -53,20 +53,31 @@ public class RayMarching2D : App {
 			float x = GetDistance(pos, shape);
 			if (x < d) d = Mathf.RoundToInt(x);
 		}
-		visuals.Add(new Shape(pos, d, 0, 1, new Vector3(246, 246, 246)));
+		visuals.Add(new Shape(pos, new Vector2(d, 0), 0, 1, new Vector3(246, 246, 246)));
 		return d;
 	}
 
-	/*private void March() {
-		
-	}*/
+	private void March() {
+		Vector2 pos = origin;
+		for (int i = 0; i < steps; i++) {
+			float d = Min(pos);
+			pos = new Vector2(
+				pos.x + d * Mathf.Cos(direction * (Mathf.PI / 180)),
+				pos.y + d * Mathf.Sin(direction * (Mathf.PI / 180))
+			);
+			if (d < 1 || d > 1000) {
+				i = steps;
+			}
+		}
+		visuals.Add(new Shape(origin, new Vector3(pos.x, pos.y, 1), 2, 0, new Vector3(246, 246, 246)));
+	}
 
 	private void InitShapes() {
 	
-		visuals.Add(new Shape(origin, 5, 0, 0, new Vector3(246, 246, 246)));
+		visuals.Add(new Shape(origin, new Vector2(5, 0), 0, 0, new Vector3(246, 246, 246)));
 		List<Shape> allShapes = objects.Concat(visuals).ToList();
 
-		shapes = new ComputeBuffer(allShapes.Count, sizeof(float) * 5 + sizeof(int) * 3);
+		shapes = new ComputeBuffer(allShapes.Count, sizeof(float) * 8 + sizeof(int) * 2);
 		shapes.SetData(allShapes);
 		
 		// reset List
@@ -76,7 +87,7 @@ public class RayMarching2D : App {
 	// Distance Estimators
 
 	private float GetDistance(Vector2 pos, Shape shape) {
-		return shape.type == 0 ? DECircle(pos, shape.pos, shape.size) : DESquare(pos, shape.pos, shape.size);
+		return shape.type == 0 ? DECircle(pos, shape.pos, shape.size[0]) : DESquare(pos, shape.pos, shape.size);
 	}
 	
 	private float Length(Vector2 a) {
@@ -87,8 +98,8 @@ public class RayMarching2D : App {
 		return Length(pos - c) - radius;
 	}
 
-	private float DESquare(Vector2 c, Vector2 pos, float size) {
-		Vector2 d = Abs(pos - c) - new Vector2(size, size);
+	private float DESquare(Vector2 c, Vector2 pos, Vector2 size) {
+		Vector2 d = Abs(pos - c) - size;
 		return Length(Vector2.Max(d, new Vector2(0, 0))) + Mathf.Min(Mathf.Max(d[0], d[1]), 0);
 	}
 
@@ -98,12 +109,12 @@ public class RayMarching2D : App {
 
 	struct Shape {
 		public Vector2 pos;
-		public int size;
-		public int type; // 0: Sphere, 1: Cube
+		public Vector3 size;
+		public int type; // 0: Sphere, 1: Cube, 2: Line
 		public int border; // 0: no, 1: yes
 		public Vector3 color;
 
-		public Shape(Vector2 pos, int size, int type, int border, Vector3 color) {
+		public Shape(Vector2 pos, Vector3 size, int type, int border, Vector3 color) {
 			this.pos = pos;
 			this.size = size;
 			this.type = type;
