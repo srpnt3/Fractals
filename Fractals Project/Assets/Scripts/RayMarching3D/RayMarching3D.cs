aaea6cfacd1cc878f5d1a4047daba0d0350559ea
VirusHD <virushd93@gmail.com>
2020-03-28 18:21:04 +0100

    Ray Marching Engine
    - created ray marching engine with compute shaders
    - rendered my first sphere
    - rendered infinite spheres lmao
    - added outline effect
    
    - and I added a random semicolon

---
+++ Fractals Project/Assets/Scripts/RayMarching3D/RayMarching3D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class RayMarching3D : App {
+
+	[ImageEffectOpaque]
+	private void OnRenderImage(RenderTexture s, RenderTexture d) {
+		
+		// init functions
+		Init();
+
+		// shader
+		shader.SetTexture(0, "Texture", tex);
+		shader.SetTexture(0, "Source", s);
+		shader.SetMatrix("CamToWorld", cam.cameraToWorldMatrix);
+		shader.SetMatrix("CamInverseProjection", cam.projectionMatrix.inverse);
+		
+		shader.Dispatch(0, Mathf.CeilToInt(w / 8), Mathf.CeilToInt(h / 8), 1);
+
+		// apply the texture
+		Graphics.Blit(tex, d);
+	}
+}
