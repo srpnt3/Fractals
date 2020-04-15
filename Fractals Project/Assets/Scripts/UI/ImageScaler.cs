5f547396cc16a689f29a48996662d84a6f90fd39
VirusHD <virushd93@gmail.com>
2020-04-15 16:57:42 +0200

    Some UI things

---
+++ Fractals Project/Assets/Scripts/UI/ImageScaler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageScaler : MonoBehaviour {
+
+	public float scale = 1f;
+
+	private RectTransform rt;
+	private RawImage img;
+	private Vector2 size;
+
+	void Start() {
+		img = GetComponent<RawImage>();
+		rt = img.rectTransform;
+		size = new Vector2(img.mainTexture.width, img.mainTexture.height);
+	}
+	
+	void Update() {
+		double screenRatio = (double) Screen.width / Screen.height;
+		double imageRatio = (double) size.x / size.y;
+	
+		// image scaling
+		if (screenRatio < imageRatio) { // use height
+			rt.sizeDelta = new Vector2(size.x * (Screen.height / size.y) * scale, Screen.height * scale);
+		} else if (screenRatio > imageRatio) { // use width
+			rt.sizeDelta = new Vector2(Screen.width * scale, size.y * (Screen.width / size.x) * scale);
+		} else {
+			rt.sizeDelta = new Vector2(Screen.width * scale, Screen.height * scale);
+		}
+	}
+}
---
+++ Fractals Project/Assets/Scripts/UI/MouseEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEffect : MonoBehaviour {
+
+	public float scale = 1;
+	public bool inverted = false;
+	public RectTransform RectTransform;
+
+	private Vector2 center;
+	private Vector2 pos;
+	private Vector2 d;
+	private Vector2 aPos;
+
+	void Start() {
+		aPos = RectTransform.anchoredPosition;
+	}
+
+	void Update() {
+		center = new Vector2(Screen.width / 2, Screen.height / 2);
+		pos = Input.mousePosition;
+		d = new Vector2((pos.x - center.x) / 40 * scale, (pos.y - center.y) / 40 * scale);
+		RectTransform.anchoredPosition = inverted ? aPos + d : aPos - d;
+	}
+}
