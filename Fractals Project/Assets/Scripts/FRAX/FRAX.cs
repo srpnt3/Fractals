1d9f8211d07b51abaa5b3390272f618b5911a6ff
VirusHD <virushd93@gmail.com>
2020-07-21 20:39:30 +0200

    FRAX
    - created and imported the spaceship

---
+++ Fractals Project/Assets/Scripts/FRAX/FRAX.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class FRAX : App {
+
+	private Material depth;
+	
+	private void Start() {
+		cameraType = CameraType.Orbit;
+		cam.depthTextureMode = DepthTextureMode.Depth;
+		depth = new Material(Shader.Find("Custom/DepthBuffer"));
+		controls.minRadius = 2;
+		controls.maxRadius = 2;
+	}
+
+	protected override void Render(RenderTexture s) {
+		
+		// write depth buffer into render texture
+		Graphics.Blit(s, tex2, depth);
+		
+		// shader
+		shader.SetTexture(0, "Texture", tex);
+		shader.SetTexture(0, "Source", tex2);
+		shader.SetMatrix("CamToWorld", cam.cameraToWorldMatrix);
+		shader.SetMatrix("CamInverseProjection", cam.projectionMatrix.inverse);
+		shader.SetFloat("zNear", cam.nearClipPlane);
+		shader.SetFloat("zFar", cam.farClipPlane);
+		
+		shader.Dispatch(0, Mathf.CeilToInt(w / 8f), Mathf.CeilToInt(h / 8f), 1);
+	}
+
+	private new void Update() {
+		base.Update();
+		ReRender();
+	}
+}
